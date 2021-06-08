1bb10450dea369c4d3ce48967985e147c356a002
purplevarun <purplevarun@gmail.com>
2021-06-08 14:03:34 +0530

    .

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
 app.listen(port,(err)=>{
 	console.log(`server started on ${port}`);
 });
 app.get('/',(req,res)=>{
-	res.send('<h1>hello world</h1>');
+	res.render('homepage');
 });
