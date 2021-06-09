46fa397e4b50f341111da7aa034a01e4fd41cea7
purplevarun <purplevarun@gmail.com>
2021-06-09 14:40:00 +0530

    .

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyparser = require('body-parser');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const mongoose = require('mongoose');
+
 app.set('view engine', 'ejs');
-app.listen(port,(err)=>{
-	console.log(`server started on ${port}`);
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(flash());
+
+mongoose.set('useUnifiedTopology',true);
+mongoose.set('useNewUrlParser',true);
+mongoose.set('useCreateIndex',true);
+mongoose.connect('mongodb://localhost',(err)=>{
+	if (err) console.log(err);
+	else console.log("Connected");
 });
+
 app.get('/', (req,res) => {
+	req.flash('hemlo');
 	res.render('homepage');
 });
 app.get ('/login', (req,res) => {
@@ -13,4 +28,7 @@ app.get ('/login', (req,res) => {
 });
 app.get ('/register', (req,res) => {
 	res.render('register');
+});
+app.listen(port,(err)=>{
+	console.log(`server started on ${port}`);
 });
