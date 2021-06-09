6ffb2e3698c1c9a68aea946687d66416be4f899f
purplevarun <purplevarun@gmail.com>
2021-06-10 02:54:16 +0530

    views\homepage.ejs

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.listen(port, () => {
-	console.log(`server running at ${port}`);
+  console.log(`server running at ${port}`);
 });
-
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-
-app.get('/', (req,res) => {
-	
+app.get("/", (req, res) => {
+  res.render("homepage");
 });
