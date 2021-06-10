1d6a6e91c64c4b8d90b266f1a1a38739fb00b835
purplevarun <purplevarun@gmail.com>
2021-06-11 01:33:40 +0530

    .

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
 var port = process.env.PORT || 3000;
+var bodyparser = require("body-parser");
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+app.use(bodyparser.urlencoded({extended:true}));
+mongoose.set('useUnifiedTopology',true);
+mongoose.set('useNewUrlParser',true);
+mongoose.set('useCreateIndex',true);
 app.listen(port, () => {
   console.log(`server running at ${port}`);
 });
@@ -16,4 +21,9 @@ app.get("/login", (req, res) => {
 });
 app.get("/register", (req, res) => {
   res.render("registerpage");
+});
+app.post ('/register', (req,res) => {
+	var data = req.body;
+	console.log(data['email']);
+	res.redirect('/register');
 });
