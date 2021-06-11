e5b59c7481a6172845e76e0f2779383c9c77e0c4
purplevarun <purplevarun@gmail.com>
2021-06-11 17:32:52 +0530

    .

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var port = process.env.PORT || 3000;
 var bodyparser = require("body-parser");
 var multer = require("multer");
 var storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "./public/uploads");
-  },
-  filename: (req, file, callback) => {
-    callback(null, Date.now() + file.originalname);
-  },
+    destination: (req, file, callback) => {
+        callback(null, "./public/uploads");
+    },
+    filename: (req, file, callback) => {
+        callback(null, Date.now() + file.originalname);
+    },
 });
 var upload = multer({ storage: storage });
 app.set("view engine", "ejs");
@@ -21,20 +21,20 @@ mongoose.set("useUnifiedTopology", true);
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useCreateIndex", true);
 app.listen(port, () => {
-  console.log(`server running at ${port}`);
+    console.log(`server running at ${port}`);
 });
 app.get("/", (req, res) => {
-  res.render("homepage");
+    res.render("homepage");
 });
 app.get("/login", (req, res) => {
-  res.render("loginpage");
+    res.render("loginpage");
 });
 app.get("/register", (req, res) => {
-  res.render("registerpage");
+    res.render("registerpage");
 });
 app.post("/register", upload.single("photo"), (req, res) => {
-  var data = req.body;
+    var data = req.body;
 
-  console.log(data);
-  res.redirect("/register");
+    console.log(data);
+    res.redirect("/register");
 });
