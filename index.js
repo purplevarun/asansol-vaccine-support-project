875ec9039daa079b85d19a46463acfe4df4010d1
purplevarun <purplevarun@gmail.com>
2021-06-11 19:39:11 +0530

    .

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var bodyparser = require("body-parser");
 var multer = require("multer");
 var passport = require("passport");
 var nodemailer = require("nodemailer");
+var express_session = require("express-session");
 var storage = multer.diskStorage({
     destination: (req, file, callback) => {
         callback(null, "./public/uploads");
@@ -15,16 +16,25 @@ var storage = multer.diskStorage({
     },
 });
 var upload = multer({ storage: storage });
+var User = require("./DataBase_Model/User");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use(
+    express_session({
+        secret: "hehe boi",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 mongoose.set("useUnifiedTopology", true);
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useCreateIndex", true);
-mongoose.connect('mongodb://localhost', (err) => {
-    if (!err)
-        console.log("Connected to Database");
+mongoose.connect("mongodb://localhost/Vaccine", (err) => {
+    if (!err) console.log("Connected to Database");
 });
 app.listen(port, () => {
     console.log(`server running at ${port}`);
@@ -40,7 +50,16 @@ app.get("/register", (req, res) => {
 });
 app.post("/register", upload.single("photo"), (req, res) => {
     var data = req.body;
-
-    console.log(data);
+    var em = data["email"];
+    var un = data["username"];
+    var pw = data["password"];
+    var pic;
+    if (req.file) {
+        console.log("dp is uploaded");
+        pic = req.file.filename;
+    } else {
+        console.log("no dp was uploaded");
+        pic = "defaultdp.jpg";
+    }
     res.redirect("/register");
 });
