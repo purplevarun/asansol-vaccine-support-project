e487b1524591b9c5e2e0e96d9e67e147f66b5f99
purplevarun <purplevarun@gmail.com>
2021-06-12 22:04:18 +0530

    .

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const bodyparser = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
+// express connection
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+// mongodb connection
 const mongourl =
-  "mongodb+srv://purplevarun:pikachu24@cluster0.emytc.mongodb.net/VaccineProject/Users";
+  "mongodb+srv://purplevarun:pikachu24@cluster0.emytc.mongodb.net/VaccineProject";
 mongoose.set("useUnifiedTopology", true);
-mongoose.set(useNewUrlParser, true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useCreateIndex", true);
 mongoose.connect(mongourl, (err) => {
   if (err) console.log(err);
   console.log("DB Connected");
