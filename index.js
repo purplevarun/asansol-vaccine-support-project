280b1d95e6cd19e8b6104c42b187ae7e5e5b5ca0
purplevarun <purplevarun@gmail.com>
2021-06-12 16:20:25 +0530

    .

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ var passport = require("passport");
 var nodemailer = require("nodemailer");
 var express_session = require("express-session");
 var storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, "./public/uploads");
-    },
-    filename: (req, file, callback) => {
-        callback(null, Date.now() + file.originalname);
-    },
+  destination: (req, file, callback) => {
+    callback(null, "./public/uploads");
+  },
+  filename: (req, file, callback) => {
+    callback(null, Date.now() + file.originalname);
+  },
 });
 var upload = multer({ storage: storage });
 var User = require("./DataBase_Model/User");
@@ -22,11 +22,11 @@ app.use(express.static("public"));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(
-    express_session({
-        secret: "hehe boi",
-        resave: false,
-        saveUninitialized: false,
-    })
+  express_session({
+    secret: "hehe boi",
+    resave: false,
+    saveUninitialized: false,
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -34,32 +34,32 @@ mongoose.set("useUnifiedTopology", true);
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useCreateIndex", true);
 mongoose.connect("mongodb://localhost/Vaccine", (err) => {
-    if (!err) console.log("Connected to Database");
+  if (!err) console.log("Connected to Database");
 });
 app.listen(port, () => {
-    console.log(`server running at ${port}`);
+  console.log(`server running at ${port}`);
 });
 app.get("/", (req, res) => {
-    res.render("homepage");
+  res.render("homepage");
 });
 app.get("/login", (req, res) => {
-    res.render("loginpage");
+  res.render("loginpage");
 });
 app.get("/register", (req, res) => {
-    res.render("registerpage");
+  res.render("registerpage");
 });
 app.post("/register", upload.single("photo"), (req, res) => {
-    var data = req.body;
-    var em = data["email"];
-    var un = data["username"];
-    var pw = data["password"];
-    var pic;
-    if (req.file) {
-        console.log("dp is uploaded");
-        pic = req.file.filename;
-    } else {
-        console.log("no dp was uploaded");
-        pic = "defaultdp.jpg";
-    }
-    res.redirect("/register");
+  var data = req.body;
+  var em = data["email"];
+  var un = data["username"];
+  var pw = data["password"];
+  var pic;
+  if (req.file) {
+    console.log("dp is uploaded");
+    pic = req.file.filename;
+  } else {
+    console.log("no dp was uploaded");
+    pic = "defaultdp.jpg";
+  }
+  res.redirect("/register");
 });
