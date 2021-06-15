b2b2dfb8a1286dcb77fa1d98c2ccc0a7641e6b99
purplevarun <purplevarun@gmail.com>
2021-06-16 04:38:17 +0530

    .

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: true }));
