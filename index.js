df8129a8d700ae4773da6d21d5ad51d3e5c717e7
purplevarun <purplevarun@gmail.com>
2021-06-16 05:52:34 +0530

    .

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
 app.use(bodyparser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/*", (req, res) => {
+  res.render("errorpage");
+});
+app.listen(port, () => {
+  console.log("server started on port " + port);
+});
