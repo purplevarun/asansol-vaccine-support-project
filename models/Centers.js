b857bee6d2bdf419e5a6d4f962d070bcc0ecabb3
purplevarun <purplevarun@gmail.com>
2021-06-18 07:03:27 +0530

    adding-new-centers

--- models/Centers.js
+++ models/Centers.js
@@ -1,13 +1,17 @@
 var mongoose = require("mongoose");
 
-var shopsSchema = new mongoose.Schema({
+var centerSchema = new mongoose.Schema({
   name: String,
   address: String,
   contact: String,
   vaccine_name: String,
+  photo: {
+    type: Buffer,
+    contentType: String,
+  },
   doses: String,
   by: String,
   created: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("Centers", shopsSchema);
+module.exports = mongoose.model("Centers", centerSchema);
